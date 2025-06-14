fa250f9b3a41d2b646cfe88bfff8039051db8f01
Shraddha0224 <23f2004944@ds.study.iitm.ac.in>
2025-06-14 09:07:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1546,6 +1546,5 @@ async def health_check():
             status_code=500,
             content={"status": "unhealthy", "error": str(e), "api_key_set": bool(API_KEY)}
         )
-#checking
-#if __name__ == "__main__":
- #   uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
