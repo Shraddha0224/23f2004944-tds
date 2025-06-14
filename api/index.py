027396fdf8521023db2db6c076e451556b90745f
Shraddha0224 <23f2004944@ds.study.iitm.ac.in>
2025-06-14 09:22:22 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
-# api/index.py
-
-from app import app  # This imports your FastAPI app from app.py
+from app import app
 from mangum import Mangum
 
-# This wraps the FastAPI app to make it compatible with Vercel's Lambda
 handler = Mangum(app)
